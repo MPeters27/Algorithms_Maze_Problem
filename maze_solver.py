bc66861982e2139bc8264c4e4c7c138038a9b091
MPeters27 <mapeters@mines.edu>
2018-11-19 14:58:53 -0700

    Added input file and file reading plan

---
+++ maze_solver.py
@@ -0,0 +1,5 @@
+#Read in first two numbers to establish boundaries
+#Read in second line to assign colors to the vertices
+#Read in third line to establish starting locations
+#Read in the remaining lines, as such: the first item is a key. It will map
+# to the two other values, an int and a char.
