3b4c957190aa0e2e0c8919d37015d38a5d5d17b9
MPeters27 <mapeters@mines.edu>
2018-11-19 19:39:41 -0700

    Paths array complete. Contains info on potential directions

--- maze_solver.py
+++ maze_solver.py
@@ -23,7 +23,36 @@ for i in range(M):
     arr = line.split(" ")
     temp = initial_graph.get(arr[0])
     if temp is None:
-        initial_graph.update({arr[0] : [arr[1], arr[2]]})
+        initial_graph.update({arr[0]: [arr[1], arr[2]]})
     else:
         initial_graph.get(arr[0]).append(arr[1])
         initial_graph.get(arr[0]).append(arr[2])
+
+# problem solving section
+visited = []
+state = (S1, S2)
+visited.append(state)
+paths = {}
+
+for i in range(1, N-1):
+    for j in range(1, N-1):
+        if i == j:
+            continue
+        else:
+            temp_paths = []
+            color_x = vertex_colors[i - 1]
+            color_y = vertex_colors[j - 1]
+            arr1 = initial_graph.get(str(i))
+            arr2 = initial_graph.get(str(j))
+
+            if color_y in arr1:
+                index = arr1.index(color_y)
+                temp_paths.append((int(arr1[index - 1]), j))
+            if color_x in arr2:
+                index = arr2.index(color_x)
+                temp_paths.append((i, int(arr2[index - 1])))
+
+            paths.update({(i, j): temp_paths})
+
+
+
